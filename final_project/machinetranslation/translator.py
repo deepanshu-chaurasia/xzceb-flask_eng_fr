120028e2ab8f4ad53a293e0fc757c466edc6ea53
deepanshu-chaurasia <chaurasia.deepanshu03@gmail.com>
2022-05-18 15:00:09 +0000

    Final changes

--- final_project/machinetranslation/translator.py
+++ final_project/machinetranslation/translator.py
@@ -6,24 +6,30 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-apikey = os.environ['apikey']
-url = os.environ['url']
-
+apikey = os.environ["apikey"]
+url = os.environ["url"]
 print(apikey)
+print(url)
 
-authenticator = IAMAuthenticator('55cG3hhvVqEUFhRjrymHkYaw6RzvobpVLXA6FY8IDpYv')
-language_translator = LanguageTranslatorV3(
-    version='2018-05-01',
-    authenticator=authenticator
-)
-language_translator.set_service_url('https://api.au-syd.language-translator.watson.cloud.ibm.com/instances/edb5e844-e53c-4094-ad92-a35e080fcc4e')
+# Translator Instance and Authentication
+authenticator = IAMAuthenticator("55cG3hhvVqEUFhRjrymHkYaw6RzvobpVLXA6FY8IDpYv")
+language_translator = LanguageTranslatorV3(version = "2018-05-01", authenticator = authenticator)
+language_translator.set_service_url("https://api.au-syd.language-translator.watson.cloud.ibm.com/instances/edb5e844-e53c-4094-ad92-a35e080fcc4e")
 
-def englishToFrench(englishText):
-    translation= language_translator.translate(text=englishText, model_id='en-fr').get_result()
-    frenchText=translation['translations'][0]['translation']
-    return frenchText
+def english_to_french(english_text):
+    """
+    Translate English to French
+    """
 
-def frenchToEnglish(frenchText):
-    translation= language_translator.translate(text=frenchText, model_id='fr-en').get_result()
-    englishText=translation['translations'][0]['translation']
-    return englishText
+    translation = language_translator.translate(text = english_text, model_id = "en-fr").get_result()
+    french_text = translation["translations"][0]["translation"]
+    return french_text
+
+def french_to_english(french_text):
+    """
+    Translates French to English
+    """
+
+    translation = language_translator.translate(text = french_text, model_id = "fr-en").get_result()
+    english_text = translation["translations"][0]["translation"]
+    return english_text
